fc572be2f2e00fa949cd3ce23cfa0d73c6c1f3b9
Jesus-06 <jesusgm06.05@gmail.com>
2022-05-08 14:02:30 +0200

    Jesus
    version 1.1

--- SentinelsGame/core/src/com/sentinels/game/MainMenuScreen.java
+++ SentinelsGame/core/src/com/sentinels/game/MainMenuScreen.java
@@ -3,16 +3,22 @@ package com.sentinels.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
-
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
 
 public class MainMenuScreen implements Screen {
 
 	final Sentinels sent;
-	
 	Texture texture;
+	Stage stage;
+	TextButton b1,b2,b3;
+	Skin skin;
+	Table table;
 	
 	public MainMenuScreen(Sentinels sentinels) {
 		
@@ -25,6 +31,22 @@ public class MainMenuScreen implements Screen {
 		sent.batch = new SpriteBatch();
 		texture = new Texture(Gdx.files.internal("fondo.jpg"));
 		
+		stage = new Stage();
+		table = new Table();
+		table.setFillParent(true);
+		skin = new Skin(Gdx.files.internal("skin/uiskin.json"));
+		b1 = new TextButton("Nuevo Juego", skin);
+		b2 = new TextButton("Cargar Juego", skin);
+		b3 = new TextButton("Salir", skin);
+		
+		table.add(b1).padBottom(50).height(40).width(130);
+		table.row();
+		table.add(b2).height(40).width(130);
+		table.row();
+		table.add(b3).padTop(50).width(130).height(40);
+		
+		stage.addActor(table);
+		Gdx.input.setInputProcessor(stage);
 	}
 
 	// Renderizar el fondo
@@ -36,6 +58,9 @@ public class MainMenuScreen implements Screen {
 		sent.batch.begin();
 		sent.batch.draw(texture, 0, 0);
 		sent.batch.end();
+		
+		stage.draw();
+		stage.act(delta);
 		
 	}
 

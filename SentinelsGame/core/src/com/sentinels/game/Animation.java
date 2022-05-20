54b9a8e079bc9b7982560ce08e5c36a2b287ccf0
Jesus-06 <jesusgm06.05@gmail.com>
2022-05-20 15:49:12 +0200

    Jesus
    Solo por cesar

--- SentinelsGame/core/src/com/sentinels/game/Animation.java
+++ SentinelsGame/core/src/com/sentinels/game/Animation.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 public class Animation {
 	protected Sprite sprite;
 	
-	//Crea el objeto con cierta textura en la posición x, y
+	//Crea el objeto con cierta textura en la posicion x, y
 	public Animation(Texture texture,float x, float y) {
 		sprite= new Sprite(texture);
 		sprite.setPosition(x, y);

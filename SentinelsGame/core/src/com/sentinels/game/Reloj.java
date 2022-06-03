bac0f2db26e2ec6957bc33a4d3a324bd36ead92b
CesarJosueJS <102241141+CesarJosueJS@users.noreply.github.com>
2022-06-03 13:48:02 +0200

    Cesar
    Asset push

---
+++ SentinelsGame/core/src/com/sentinels/game/Reloj.java
@@ -0,0 +1,25 @@
+package com.sentinels.game;
+
+public class Reloj extends Thread{
+	//Construye un nuevo hilo.
+
+	Reloj(String nombre){
+		//super se usa para llamar aj versión del constructor de Thread
+		super(nombre);
+
+	}
+	//Punto de entrada del hilo
+	public void run(){
+			try  {
+				while (true) {
+					PantallaJuego.stateTime ++;
+					Thread.sleep(70);
+					if(PantallaJuego.stateTime==37){
+						PantallaJuego.stateTime=0;
+					}
+				}
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+	}
+}

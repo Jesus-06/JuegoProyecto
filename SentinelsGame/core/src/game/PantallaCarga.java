21245eafa770214a493b7f007f20bcd63664806f
Jesus-06 <jesusgm06.05@gmail.com>
2022-06-06 08:43:28 +0200

    Jesus
    Pantalla de carga terminada

--- SentinelsGame/core/src/game/PantallaCarga.java
+++ SentinelsGame/core/src/game/PantallaCarga.java
@@ -1,9 +1,39 @@
 package game;
 
-import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.scenes.scene2d.ui.Container;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 
-public class PantallaCarga implements Screen {
+public class PantallaCarga extends SettingsScreen {
+
+    public Texture fondo, personaje, play;
+    public Skin skin;
+    public Table table;
+
     public PantallaCarga(Sentinels sent) {
+
+        super(sent);
+
+        table = new Table();
+        table.setFillParent(true);
+
+        fuente = new BitmapFont(Gdx.files.internal("skin2/estilo1.fnt"),
+                Gdx.files.internal("skin2/estilo1.png"),false);
+
+
+        fondo = new Texture(Gdx.files.internal("fondo_gameLoad.png"));
+        personaje = new Texture(Gdx.files.internal("Night/NightEnormeIzq.png"));
+
+        stage.addActor(table);
+        Gdx.input.setInputProcessor(stage);
     }
 
     @Override
@@ -15,6 +45,20 @@ public class PantallaCarga implements Screen {
     @Override
     public void render(float delta) {
 
+        Gdx.gl.glClearColor(0.1f, 0.1f, 0.1f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        spBatch.begin();
+        spBatch.draw(fondo,500,50);
+        spBatch.draw(personaje,150,450);
+        fuente.draw(spBatch,"PERFILES", 730,725);
+        fuente.draw(spBatch,"PARTIDA", 1100,725);
+        spBatch.end();
+
+        stage.draw();
+        stage.act(delta);
+
+        table.debug();
     }
 
     @Override
